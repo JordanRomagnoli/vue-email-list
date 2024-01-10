427fd48cb2cfc2123e1a51a3b84be8baf114debd
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-01-10 14:59:57 +0100

    - FINISH - w/ bonus

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,28 @@
-// Test funzionamento
-console.log('Template');
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            
+            randomEmail : []
+
+        }
+    },
+    methods : {
+
+    },
+    mounted() {
+
+        for(let i = 0; i < 10; i++){
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+
+                let newEmail = res.data.response;
+                this.randomEmail.push(newEmail);
+    
+            })
+        }
+        console.log(this.randomEmail)
+
+    }
+}).mount('#app')
